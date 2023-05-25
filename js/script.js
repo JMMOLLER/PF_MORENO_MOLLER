39df1794324a56270036e1ad1dada99609feec83
Jorge M'Moller <jlmm.jlmm76@gmail.com>
2023-05-24 21:15:19 -0500

    Content hamburguer working
    El contenido de hamburguer ahora está funcionando correctamente

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log("Hello World");
+const HAMBURGUER = document.getElementById("hamburguer");
+const NAV_CONTAINER = document.querySelector(".content-nav-mobile");
+const TOP_SPAN = document.querySelector(".hamburguer span:nth-child(1)")
+const MIDDLE_SPAN = document.querySelector(".hamburguer span:nth-child(2)")
+const BOTTOM_SPAN = document.querySelector(".hamburguer span:nth-child(3)")
+
+HAMBURGUER.onclick = () => {
+    const IS_TOGGLED = NAV_CONTAINER.classList.toggle("active");
+    if(!IS_TOGGLED){
+        TOP_SPAN.style.animation = "hamburguer-top-reverse .2s ease forwards"
+        MIDDLE_SPAN.style.animation = "hamburguer-middle-reverse .2s ease forwards"
+        BOTTOM_SPAN.style.animation = "hamburguer-bottom-reverse .2s ease forwards"
+        NAV_CONTAINER.style.animation = "hiddenNav .5s ease forwards";
+    }else{
+        TOP_SPAN.style.animation = "hamburguer-top .2s ease forwards"
+        MIDDLE_SPAN.style.animation = "hamburguer-middle .2s ease .1s forwards"
+        BOTTOM_SPAN.style.animation = "hamburguer-bottom .2s ease .1s forwards"
+        NAV_CONTAINER.style.animation = "showNav .5s ease forwards";
+    }
+}
